26440f28778efc1ee8ecd57db83b77587564beeb
antonioferrigno28 <ferrigno1@hotmail.it>
2024-10-16 14:43:29 +0200

    Bonus easy solutions (not done)

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -1,19 +1,21 @@
-const teachers = [
-  'Nathan',
-  'Ed',
-  'Fabio',
-  'Phil',
-  'Carlo',
-  'Lewis',
-  'Luca'
-]; // NON MODIFICARE QUESTA VARIABILE
+const teachers = ["Nathan", "Ed", "Fabio", "Phil", "Carlo", "Lewis", "Luca"]; // NON MODIFICARE QUESTA VARIABILE
 
 // 1. Inverti l'ordine degli insegnanti nell'array teachers
 // e salva il risultato nella variabile reversedTeachers
-const reversedTeachers = null;
+
+//SOLUZIONE EASY
+//const reversedTeachers = teachers.reverse();
+//console.log(reversedTeachers);
 
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
-const longNames = null;
+
+//SOLUZIONE EASY
+//const longNames = teachers.filter((teacher) => teacher.length >= 5);
+//console.log(longNames);
 
 // 3. Rimuovi 'Ed' dall'array teachers
+
+//SOLUZIONE EASY
+//teachers.splice(1, 1);
+//console.log(teachers);
