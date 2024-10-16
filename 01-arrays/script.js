9bdff6f3a7b8601884355b6e5c334fc46b13761f
antonioferrigno28 <ferrigno1@hotmail.it>
2024-10-16 14:31:25 +0200

    Block 1 done!

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -1,5 +1,8 @@
 const teachers = ["Nathan", "Ed", "Fabio", "Phil", "Carlo", "Lewis", "Luca"]; // NON MODIFICARE QUESTA VARIABILE
 
+//CONSOLE LOG AGGIUNTO DA ME
+console.log(teachers); // STAMPO I VALORI INIZIALI DELL'ARRAY PER TENERLI DAVANTI INVARIATI
+
 // 1. Utilizzando un indice sull'array teachers, cambia il valore della
 // variabile fourthTeacher qui sotto in modo che sia il quarto insegnante elencato nell'array
 const fourthTeacher = teachers[3];
