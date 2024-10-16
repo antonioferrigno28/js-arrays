4cd6100dc6b763c332dae4f2af1ce8c7bfc41580
antonioferrigno28 <ferrigno1@hotmail.it>
2024-10-16 14:59:26 +0200

    Error "undefined" bonus 1

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -3,6 +3,13 @@ const teachers = ["Nathan", "Ed", "Fabio", "Phil", "Carlo", "Lewis", "Luca"]; //
 // 1. Inverti l'ordine degli insegnanti nell'array teachers
 // e salva il risultato nella variabile reversedTeachers
 
+//SOLUZIONE CICLO FOR
+let teachersReverse = [];
+for (let i = teachers.length; i >= 0; i--) {
+  teachersReverse.push(teachers[i]);
+}
+console.log(teachersReverse);
+
 //SOLUZIONE EASY
 //const reversedTeachers = teachers.reverse();
 //console.log(reversedTeachers);
@@ -10,11 +17,15 @@ const teachers = ["Nathan", "Ed", "Fabio", "Phil", "Carlo", "Lewis", "Luca"]; //
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
 
+//SOLUZIONE CICLO FOR
+
 //SOLUZIONE EASY
 //const longNames = teachers.filter((teacher) => teacher.length >= 5);
 //console.log(longNames);
 
 // 3. Rimuovi 'Ed' dall'array teachers
+
+//SOLUZIONE CICLO FOR
 
 //SOLUZIONE EASY
 //teachers.splice(1, 1);
